f417fdbf91f87e3dd6c8b65daa1d3d205263acca
Mia Hartsfield <70355320+miahartsfield@users.noreply.github.com>
2022-04-20 09:58:49 -0500

    new folders

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 
 <!doctype html>
 <html lang="en">
@@ -77,16 +80,22 @@ if (!isset($_SESSION['username'])) {
             <li><a href="#">About me</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
-        <div>
-            <form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="Username/E-mail...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">Signup</a>
-            <form action="includes/logout.inc.php" method="post">
-                <button type="submit" name="logout-submit">Logout</button>
-            </form>
+        <div class="header-login">
+            <?php
+                if(isset($_SESSION['userId'])){
+                    echo '<form action="includes/logout.inc.php" method="post">
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>';
+                }
+                else {
+                  echo '<form action="includes/login.inc.php" method="post">
+                  <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                  <input type="password" name="pwd" placeholder="Password...">
+                  <button type="submit" name="login-submit">Login</button>
+              </form>
+              <a href="signup.php">Signup</a>';
+                }
+            ?>
         </div>
     </nav>  
 </header>
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -8,7 +8,7 @@ if (isset($_POST['signup-submit'])){
     $password = $_POST['pwd'];
     $passwordRepeat = $_POST['pwd-repeat'];
 
-    if (empty($username) || empty($username) || empty($username) || empty($username)){
+    if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)){
         header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=" .$email);
         exit();
     }
@@ -22,19 +22,19 @@ if (isset($_POST['signup-submit'])){
         exit();
     }
     else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        header("Location: ../signup.php?error=passwordcheck&mail=".$username."&mail=" .$email);
+        header("Location: ../signup.php?error=passwordcheck&mail=".$email);
         exit();
     }
     else if ($password !== $passwordRepeat){
-        header("Location: ../signup.php?error=passwordcheck&mail=".$username."&mail=" .$email);
+        header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=" .$email);
         exit();
     }
     else {
 
-        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?AND pwdUsers=?";
+        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
         $stmt= mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("location: ../signup.php?error=sqlerror");
+            header("Location: ../signup.php?error=sqlerror");
             exit();
      }
     else {
@@ -42,6 +42,34 @@ if (isset($_POST['signup-submit'])){
         mysqli_stmt_execute($stmt);
         mysqli_stmt_store_result($stmt);
         $resultCheck = mysqli_stmt_num_rows($stmt);
+        if ($resultCheck > 0) {
+            header("Location: ../signup.php?error=usertaken&mail=".$email);
+            exit();
+        }
+       else{
+         $sql ="INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+         $stmt= mysqli_stmt_init($conn);
+         if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+       }
+       else{
+        $hashedPwd= password_hash($password, PASSWORD_DEFAULT);
+
+        mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+        mysqli_stmt_execute($stmt);
+        header("Location: ../signup.php?signup=success");
+            exit();
+       }
     }
-    
-}
+    }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+    }
+
+    else{
+        header("location: ../signup.php");
+            exit();
+    }
